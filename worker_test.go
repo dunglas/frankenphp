3ae5356d30cb35161aaac7a97aa81ba1931d4a4e
Kévin Dunglas <kevin@dunglas.fr>
2022-05-18 10:08:36 +0200

    improve tests

--- worker_test.go
+++ worker_test.go
@@ -1,6 +1,7 @@
 package frankenphp_test
 
 import (
+	"fmt"
 	"io"
 	"net/http/httptest"
 	"net/url"
@@ -11,35 +12,31 @@ import (
 )
 
 func TestWorker(t *testing.T) {
-	shutdown, handler, _ := createTestHandler(t, "worker.php")
+	shutdown, handler, iterations := createTestHandler(t, "worker.php")
 	defer shutdown()
 
-	/*for i := 0; i < iterations; i++ {*/
-	formData := url.Values{"baz": {"bat"}}
-	req := httptest.NewRequest("POST", "http://example.com/worker.php?foo=bar", strings.NewReader(formData.Encode()))
-	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-	w := httptest.NewRecorder()
-	handler(w, req)
+	for i := 0; i < iterations; i++ {
+		formData := url.Values{"baz": {"bat"}}
+		req := httptest.NewRequest("POST", "http://example.com/worker.php?foo=bar", strings.NewReader(formData.Encode()))
+		req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+		w := httptest.NewRecorder()
+		handler(w, req)
 
-	resp := w.Result()
-	body, _ := io.ReadAll(resp.Body)
+		resp := w.Result()
+		body, _ := io.ReadAll(resp.Body)
 
-	t.Log(string(body))
+		assert.Contains(t, string(body), fmt.Sprintf("Requests handled: %d", i*2))
 
-	assert.Contains(t, string(body), "Requests handled: 0")
+		formData2 := url.Values{"baz2": {"bat2"}}
+		req2 := httptest.NewRequest("POST", "http://example.com/worker.php?foo2=bar2", strings.NewReader(formData2.Encode()))
+		req2.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 
-	formData2 := url.Values{"baz2": {"bat2"}}
-	req2 := httptest.NewRequest("POST", "http://example.com/worker.php?foo2=bar2", strings.NewReader(formData2.Encode()))
-	req2.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+		w2 := httptest.NewRecorder()
+		handler(w2, req2)
 
-	w2 := httptest.NewRecorder()
-	handler(w2, req2)
+		resp2 := w2.Result()
+		body2, _ := io.ReadAll(resp2.Body)
 
-	resp2 := w2.Result()
-	body2, _ := io.ReadAll(resp2.Body)
-
-	t.Log(string(body2))
-
-	assert.Contains(t, string(body2), "Requests handled: 1")
-	/*}*/
+		assert.Contains(t, string(body2), fmt.Sprintf("Requests handled: %d", i*2+1))
+	}
 }
