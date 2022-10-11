3712d0d69b5fe5d618d0afb4790f524f740cb674
Kévin Dunglas <kevin@dunglas.fr>
2022-10-11 10:54:30 +0200

    test: add tests for exception handling (#11)

--- testdata/exception.php
+++ testdata/exception.php
@@ -2,9 +2,7 @@
 
 require_once __DIR__.'/_executor.php';
 
-throw new Exception('unexpected');
-
 return function () {
     echo 'hello';
-    throw new Exception('error');
+    throw new Exception("request {$_GET['i']}");
 };
