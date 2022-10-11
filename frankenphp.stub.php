5af6b10d1fef3b6f6fb81d20695f5713d3db54dc
Kévin Dunglas <kevin@dunglas.fr>
2022-10-11 17:40:12 +0200

    feat: add support for 103 Early Hints (#12)

--- frankenphp.stub.php
+++ frankenphp.stub.php
@@ -3,3 +3,5 @@
 /** @generate-class-entries */
 
 function frankenphp_handle_request(callable $callback): bool {}
+
+function headers_send(int $status = 200): int {}
--- frankenphp_arginfo.h
+++ frankenphp_arginfo.h
@@ -1,15 +1,21 @@
 /* This is a generated file, edit the .stub.php file instead.
- * Stub hash: f2e8375a3acb3080fc6c329eafc47155f0702c85 */
+ * Stub hash: f9ead962eae043fa397a4e573e8905876b7b390b */
 
 ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_frankenphp_handle_request, 0, 1, _IS_BOOL, 0)
 	ZEND_ARG_TYPE_INFO(0, callback, IS_CALLABLE, 0)
 ZEND_END_ARG_INFO()
 
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_headers_send, 0, 0, IS_LONG, 0)
+	ZEND_ARG_TYPE_INFO_WITH_DEFAULT_VALUE(0, status, IS_LONG, 0, "200")
+ZEND_END_ARG_INFO()
+
 
 ZEND_FUNCTION(frankenphp_handle_request);
+ZEND_FUNCTION(headers_send);
 
 
 static const zend_function_entry ext_functions[] = {
 	ZEND_FE(frankenphp_handle_request, arginfo_frankenphp_handle_request)
+	ZEND_FE(headers_send, arginfo_headers_send)
 	ZEND_FE_END
 };
