18940108d1023c2e44f20b08924876b2ef440c63
Dylan Blokhuis <dylanblokhuis3@hotmail.com>
2022-10-28 17:25:02 +0200

    feat: add support for $_SERVER['PHP_SELF'] (#71)
    * PHP_SELF
    
    * remove accidental formatting
    
    * test: add test for PHP_SELF
    
    Co-authored-by: Kévin Dunglas <kevin@dunglas.fr>

--- cgi.go
+++ cgi.go
@@ -86,7 +86,10 @@ func populateEnv(request *http.Request) error {
 	if scriptName != "" && !strings.HasPrefix(scriptName, "/") {
 		scriptName = "/" + scriptName
 	}
-
+	
+	if _, ok := fc.Env["PHP_SELF"]; !ok {
+		fc.Env["PHP_SELF"] = fpath
+	}
 	if _, ok := fc.Env["DOCUMENT_URI"]; !ok {
 		fc.Env["DOCUMENT_URI"] = docURI
 	}
