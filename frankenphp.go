e514823faaa1697307e53b2157c0fc027349435e
Kévin Dunglas <kevin@dunglas.fr>
2022-12-19 22:09:15 +0100

    fix: use SA_ONSTACK in C-Thread-Pool

--- frankenphp.go
+++ frankenphp.go
@@ -6,7 +6,7 @@
 package frankenphp
 
 //go:generate rm -Rf C-Thread-Pool/
-//go:generate git clone --depth=1 git@github.com:Pithikos/C-Thread-Pool.git
+//go:generate git clone --branch=fix/SA_ONSTACK --depth=1 git@github.com:dunglas/C-Thread-Pool.git
 //go:generate rm -Rf C-Thread-Pool/.git C-Thread-Pool/.circleci C-Thread-Pool/docs C-Thread-Pool/tests
 
 // #cgo CFLAGS: -Wall -Werror
