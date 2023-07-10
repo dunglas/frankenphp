b57ebf52e3d395928a49116c2dd649aed398ff6b
Kévin Dunglas <kevin@dunglas.fr>
2023-07-10 17:38:29 +0200

    better fix for flush.php

--- testdata/flush.php
+++ testdata/flush.php
@@ -1,11 +1,16 @@
 <?php
 
+
 require_once __DIR__.'/_executor.php';
 
 return function () {
+    if (ini_get("output_buffering") !== "0") {
+        // Disable output buffering if not already done
+        while (@ob_end_flush());
+    }
+
     echo 'He';
 
-    ob_flush();
     flush();
 
     echo 'llo '.($_GET['i'] ?? '');
