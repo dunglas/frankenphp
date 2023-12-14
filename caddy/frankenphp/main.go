e20de214c55eda9c44713cdb55464552e5f16e90
Kévin Dunglas <kevin@dunglas.fr>
2023-12-14 14:41:34 +0100

    chore: improve automaxprocs logs

--- caddy/frankenphp/main.go
+++ caddy/frankenphp/main.go
@@ -1,10 +1,10 @@
-// Copied from https://github.com/caddyserver/xcaddy/blob/b7fd102f41e12be4735dc77b0391823989812ce8/environment.go#L251
 package main
 
 import (
 	"github.com/caddyserver/caddy/v2"
 	caddycmd "github.com/caddyserver/caddy/v2/cmd"
 	"go.uber.org/automaxprocs/maxprocs"
+	"go.uber.org/zap"
 
 	// plug in Caddy modules here.
 	_ "github.com/caddyserver/caddy/v2/modules/standard"
@@ -13,11 +13,12 @@ import (
 	_ "github.com/dunglas/vulcain/caddy"
 )
 
-func init() {
-	//nolint:errcheck
-	maxprocs.Set(maxprocs.Logger(caddy.Log().Sugar().Debugf))
-}
-
 func main() {
+	undo, err := maxprocs.Set()
+	defer undo()
+	if err != nil {
+		caddy.Log().Warn("failed to set GOMAXPROCS", zap.Error(err))
+	}
+
 	caddycmd.Main()
 }
