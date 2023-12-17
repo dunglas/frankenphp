b5fe416b29b117534a036da208fc69fb8c196190
Kévin Dunglas <kevin@dunglas.fr>
2023-12-17 11:26:02 +0100

    fix perms

--- reload_test.sh
+++ reload_test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for ((i = 0 ; i < 100 ; i++)); do
-    curl http://localhost:2019/config/apps/frankenphp
-    curl -H 'Cache-Control: must-revalidate' --json '{"workers":[{"file_name":"./index.php"}]}' -X PATCH http://localhost:2019/config/apps/frankenphp
+    curl -sS -o /dev/null http://localhost:2019/config/apps/frankenphp
+    curl -sS -o /dev/null -H 'Cache-Control: must-revalidate' -H 'Content-Type: application/json' --data-binary '{"workers":[{"file_name":"./index.php"}]}' -X PATCH http://localhost:2019/config/apps/frankenphp
 done
