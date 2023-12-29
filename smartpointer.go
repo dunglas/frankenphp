d236d1b5ba42d7edcb83ea2d556819886c460fb2
Robert Landers <landers.robert@gmail.com>
2023-12-29 10:08:03 +0100

    try inlining

--- smartpointer.go
+++ smartpointer.go
@@ -12,8 +12,6 @@ FrankenPHP is fairly complex because it shuffles handles/requests/contexts
 between C and Go. This simplifies the lifecycle management of per-request
 structures by allowing us to hold references until the end of the request
 and ensure they are always cleaned up.
-
-Note the non-inlined functions. Not inlining allows for slightly faster execution in this case due to
 */
 
 // PointerList A list of pointers that can be freed at a later time
@@ -32,8 +30,6 @@ func (h *handleList) AddHandle(handle cgo.Handle) {
 }
 
 // AddPointer Call when creating a request-level C pointer for the very first time
-//
-//go:noinline
 func (p *pointerList) AddPointer(ptr unsafe.Pointer) {
 	p.Pointers = append(p.Pointers, ptr)
 }
@@ -41,8 +37,6 @@ func (p *pointerList) AddPointer(ptr unsafe.Pointer) {
 // AddString adds a string to the pointer list by converting it to a C-char pointer and calling the AddPointer method.
 // The string is converted to a C-char pointer using the C.CString function.
 // It is recommended to use this method when you need to add a string to the pointer list.
-//
-//go:noinline
 func (p *pointerList) AddString(str *C.char) {
 	p.AddPointer(unsafe.Pointer(str))
 	//getLogger().Warn("Adding string", zap.Int("i", len(p.Pointers)), zap.String("str", C.GoString(str)), zap.Stack("trace"))
@@ -50,8 +44,6 @@ func (p *pointerList) AddString(str *C.char) {
 
 // ToCString takes a string and converts it to a C string using C.CString. Then it calls the AddString method of
 // pointerList to add the resulting C string as a pointer to the pointer
-//
-//go:noinline
 func (p *pointerList) ToCString(string string) *C.char {
 	str := C.CString(string)
 	p.AddString(str)
