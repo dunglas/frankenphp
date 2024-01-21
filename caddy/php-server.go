0875d4e379ad9f62805f3751c5c1b61e06505445
Pierre du Plessis <pierre@pcservice.co.za>
2024-01-21 09:36:23 +0200

    Load custom Caddyfile from embedded app if it exists

--- caddy/php-server.go
+++ caddy/php-server.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"os"
 	"path/filepath"
 	"strconv"
 	"strings"
@@ -88,6 +89,25 @@ func cmdPHPServer(fs caddycmd.Flags) (int, error) {
 	}
 
 	if frankenphp.EmbeddedAppPath != "" {
+
+		if _, err := os.Stat(filepath.Join(frankenphp.EmbeddedAppPath, "Caddyfile")); err == nil {
+			config, _, err := caddycmd.LoadConfig(filepath.Join(frankenphp.EmbeddedAppPath, "Caddyfile"), "")
+
+			if err != nil {
+				return caddy.ExitCodeFailedStartup, err
+			}
+
+			err = caddy.Load(config, true)
+
+			if err != nil {
+				return caddy.ExitCodeFailedStartup, err
+			}
+
+			select {}
+
+			return caddy.ExitCodeSuccess, nil
+		}
+
 		if root == "" {
 			root = filepath.Join(frankenphp.EmbeddedAppPath, defaultDocumentRoot)
 		} else if filepath.IsLocal(root) {
