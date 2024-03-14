676945f38af422caa69551f578baa377c80b9b89
Kévin Dunglas <kevin@dunglas.fr>
2024-03-14 23:13:17 +0100

    feat: use mimalloc for static builds

--- build-static.sh
+++ build-static.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 set -o errexit
+set -x
 
 if ! type "git" > /dev/null; then
     echo "The \"git\" command must be installed."
@@ -15,6 +16,11 @@ if [ "${os}" = "darwin" ]; then
     md5binary="md5 -q"
 fi
 
+if [ "${os}" = "linux" ] && ! type "cmake" > /dev/null; then
+    echo "The \"cmake\" command must be installed."
+    exit 1
+fi
+
 if [ -z "${PHP_EXTENSIONS}" ]; then
     export PHP_EXTENSIONS="apcu,bcmath,bz2,calendar,ctype,curl,dba,dom,exif,fileinfo,filter,gd,iconv,igbinary,intl,ldap,mbregex,mbstring,mysqli,mysqlnd,opcache,openssl,pcntl,pdo,pdo_mysql,pdo_pgsql,pdo_sqlite,pgsql,phar,posix,readline,redis,session,simplexml,sockets,sodium,sqlite3,sysvsem,tokenizer,xml,xmlreader,xmlwriter,zip,zlib"
 fi
@@ -23,7 +29,7 @@ if [ -z "${PHP_EXTENSION_LIBS}" ]; then
     export PHP_EXTENSION_LIBS="bzip2,freetype,libavif,libjpeg,liblz4,libwebp,libzip"
 fi
 
-# the Brotli library must always be built as it is required by http://github.com/dunglas/caddy-cbrotli
+# The Brotli library must always be built as it is required by http://github.com/dunglas/caddy-cbrotli
 if ! echo "${PHP_EXTENSION_LIBS}" | grep -q "\bbrotli\b"; then
     export PHP_EXTENSION_LIBS="${PHP_EXTENSION_LIBS},brotli"
 fi
@@ -117,26 +123,54 @@ if [ "${os}" = "mac" ]; then
     export CGO_LDFLAGS="-framework CoreFoundation -framework SystemConfiguration"
 fi
 
-CGO_LDFLAGS="${CGO_LDFLAGS} ${PWD}/buildroot/lib/libbrotlicommon.a ${PWD}/buildroot/lib/libbrotlienc.a ${PWD}/buildroot/lib/libbrotlidec.a $(./buildroot/bin/php-config --ldflags) $(./buildroot/bin/php-config --libs)"
+CGO_LDFLAGS="${CGO_LDFLAGS} ${PWD}/buildroot/lib/libbrotlicommon.a ${PWD}/buildroot/lib/libbrotlienc.a ${PWD}/buildroot/lib/libbrotlidec.a $(./buildroot/bin/php-config --ldflags || true) $(./buildroot/bin/php-config --libs || true)"
 export CGO_LDFLAGS
 
 LIBPHP_VERSION="$(./buildroot/bin/php-config --version)"
 export LIBPHP_VERSION
 
-cd ../..
+cd ../
+
+if [ "${os}" = "linux" ]; then
+    # Replace mallocng by mimalloc
+    # The default musl allocator is slow, especially when used by multi-threaded apps,
+    # and triggers weird bugs
+    if [ ! -f "mimalloc/out/release/libmimalloc.a" ]; then
+        if [ -d "static-php-cli/" ]; then
+            cd mimalloc/
+            git pull
+        else
+            git clone https://github.com/microsoft/mimalloc.git
+            cd mimalloc/
+        fi
+
+        git checkout "$(git describe --tags "$(git rev-list --tags --max-count=1 || true)" || true)"
+        mkdir -p out/release/
+        cd out/release/
+        if [ -n "${DEBUG_SYMBOLS}" ]; then
+            cmake -DCMAKE_BUILD_TYPE=Debug ../..
+        else
+            cmake ../..
+        fi
+        make -j"$(nproc || true)"
+
+        cd ../../../
+    fi
+
+    # Also increase the default stack size to prevents issues with code including many files such as Symfony containers
+    extraExtldflags="-Wl,--push-state,--whole-archive,${PWD}/mimalloc/out/release/libmimalloc.a,--pop-state,-z,stack-size=0x80000"
+fi
+
+if [ -z "${DEBUG_SYMBOLS}" ]; then
+    extraLdflags="-w -s"
+fi
+
+cd ../
 
 # Embed PHP app, if any
 if [ -n "${EMBED}" ] && [ -d "${EMBED}" ]; then
     tar -cf app.tar -C "${EMBED}" .
     ${md5binary} app.tar > app_checksum.txt
-fi
-
-if [ "${os}" = "linux" ]; then
-    extraExtldflags="-Wl,-z,stack-size=0x80000"
-fi
-
-if [ -z "${DEBUG_SYMBOLS}" ]; then
-    extraLdflags="-w -s"
 fi
 
 cd caddy/frankenphp/
