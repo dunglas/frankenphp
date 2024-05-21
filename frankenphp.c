dd2926c53bd964062609057ab9a576ce1329c53b
Kévin Dunglas <kevin@dunglas.fr>
2024-05-21 10:17:33 +0200

    Update frankenphp.c
    Co-authored-by: Tim Düsterhus <timwolla@googlemail.com>

--- frankenphp.c
+++ frankenphp.c
@@ -120,7 +120,7 @@ static void frankenphp_request_reset() {
       php_hash_environment();
     } else {
       ctx->worker_http_globals = (zval **)malloc(sizeof(zval) * NUM_TRACK_VARS);
-      memcpy(ctx->worker_http_globals, &PG(http_globals),
+      memcpy(ctx->worker_http_globals, PG(http_globals),
              sizeof(zval) * NUM_TRACK_VARS);
     }
   }
