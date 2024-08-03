a1b9f9bd17634f16d55793a6923dfc5e2d7d7aff
Kévin Dunglas <kevin@dunglas.fr>
2024-08-03 10:09:15 +0200

    fix

--- release.sh
+++ release.sh
@@ -9,25 +9,25 @@ set -o errtrace
 set -o pipefail
 set -o xtrace
 
-if ! type "git" > /dev/null; then
-    echo "The \"git\" command must be installed."
-    exit 1
+if ! type "git" >/dev/null; then
+  echo "The \"git\" command must be installed."
+  exit 1
 fi
 
-if ! type "gh" > /dev/null; then
-    echo "The \"gh\" command must be installed."
-    exit 1
+if ! type "gh" >/dev/null; then
+  echo "The \"gh\" command must be installed."
+  exit 1
 fi
 
 if [[ $# -ne 1 ]]; then
-    echo "Usage: ./release.sh version" >&2
-    exit 1
+  echo "Usage: ./release.sh version" >&2
+  exit 1
 fi
 
 # Adapted from https://semver.org/#is-there-a-suggested-regular-expression-regex-to-check-a-semver-string
 if [[ ! $1 =~ ^(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)(-((0|[1-9][0-9]*|[0-9]*[a-zA-Z-][0-9a-zA-Z-]*)(\.(0|[1-9][0-9]*|[0-9]*[a-zA-Z-][0-9a-zA-Z-]*))*))?(\+([0-9a-zA-Z-]+(\.[0-9a-zA-Z-]+)*))?$ ]]; then
-    echo "Invalid version number: $1" >&2
-    exit 1
+  echo "Invalid version number: $1" >&2
+  exit 1
 fi
 
 git checkout main
@@ -44,6 +44,6 @@ git tag -s -m "Version $1" "caddy/v$1"
 git push --follow-tags
 
 tags=$(git tag --list --sort=-version:refname 'v*')
-previous_tag=$(awk 'NR==2 {print;exit}' <<< "${tags}")
+previous_tag=$(awk 'NR==2 {print;exit}' <<<"${tags}")
 
 gh release create --draft --generate-notes --latest --notes-start-tag "${previous_tag}" --verify-tag "v$1"
