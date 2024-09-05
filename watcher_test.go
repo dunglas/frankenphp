c946de956c59a372e746bd5918f96020062ffa60
a.stecher <a.stecher@sportradar.com>
2024-09-05 11:24:47 +0200

    Asserts instead of panicking.

--- watcher_test.go
+++ watcher_test.go
@@ -26,7 +26,7 @@ func TestWorkerShouldReloadOnMatchingPattern(t *testing.T) {
 		assert.Equal(t, "requests:1", body)
 
 		// now we verify that updating a .txt file does not cause a reload
-		requestBodyHasReset := pollForWorkerReset(handler, maxTimesToPollForChanges)
+		requestBodyHasReset := pollForWorkerReset(t, handler, maxTimesToPollForChanges)
 
 		assert.True(t, requestBodyHasReset)
 	}, &testOptions{nbParrallelRequests: 1, nbWorkers: 1, workerScript: "worker-with-watcher.php", watch:filePattern})
@@ -41,7 +41,7 @@ func TestWorkerShouldNotReloadOnNonMatchingPattern(t *testing.T) {
 		assert.Equal(t, "requests:1", body)
 
 		// now we verify that updating a .json file does not cause a reload
-		requestBodyHasReset := pollForWorkerReset(handler, minTimesToPollForChanges)
+		requestBodyHasReset := pollForWorkerReset(t, handler, minTimesToPollForChanges)
 
 		assert.False(t, requestBodyHasReset)
 
@@ -59,9 +59,9 @@ func fetchBody(method string, url string, handler func(http.ResponseWriter, *htt
 	return string(body)
 }
 
-func pollForWorkerReset(handler func(http.ResponseWriter, *http.Request), limit int) bool{
+func pollForWorkerReset(t *testing.T, handler func(http.ResponseWriter, *http.Request), limit int) bool{
 	for i := 0; i < limit; i++ {
-		updateTestFile("./testdata/files/test.txt", "updated")
+		updateTestFile("./testdata/files/test.txt", "updated", t)
 		time.Sleep(pollingTime * time.Millisecond)
 		body := fetchBody("GET", "http://example.com/worker-with-watcher.php", handler)
 		if(body == "requests:1") {
@@ -71,20 +71,15 @@ func pollForWorkerReset(handler func(http.ResponseWriter, *http.Request), limit 
 	return false;
 }
 
-func updateTestFile(fileName string, content string){
+func updateTestFile(fileName string, content string, t *testing.T){
 	absFileName, err := filepath.Abs(fileName)
-	if err != nil {
-		panic(err)
-	}
+	assert.NoError(t, err)
 	dirName := filepath.Dir(absFileName)
 	if _, err := os.Stat(dirName); os.IsNotExist(err) {
 		err = os.MkdirAll(dirName, 0700)
-		if(err != nil) {
-			panic(err)
-		}
+		assert.NoError(t, err)
 	}
 	bytes := []byte(content)
-	if err := os.WriteFile(absFileName, bytes, 0644); err != nil {
-		panic(err)
-	}
+	err = os.WriteFile(absFileName, bytes, 0644)
+	assert.NoError(t, err)
 }
