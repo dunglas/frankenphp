5de7a3dd0c5b402fcdd6122085b50f07797b3fdc
a.stecher <a.stecher@sportradar.com>
2024-09-07 10:22:34 +0200

    Refactors sessions and options into a struct.

--- watcher.go
+++ watcher.go
@@ -7,41 +7,76 @@ import (
 	"sync/atomic"
 )
 
+type watcher struct {
+	sessions   []*fswatch.Session
+	watchOpts  []*watchOpt
+	workerOpts []workerOpt
+}
+
 var (
-	// TODO: combine session and watchOpt into a struct
-	watchSessions []*fswatch.Session
-	// we block reloading until workers have stopped
+	// the currently active file watcher
+	activeWatcher *watcher
+	// reloading is blocked if a reload is queued
 	blockReloading atomic.Bool
 	// when stopping the watcher we need to wait for reloading to finish
 	reloadWaitGroup sync.WaitGroup
-	// active watch options that need to be disabled on shutdown
-	activeWatchOpts []*watchOpt
 )
 
 func initWatcher(watchOpts []watchOpt, workerOpts []workerOpt) error {
 	if len(watchOpts) == 0 || len(workerOpts) == 0 {
 		return nil
 	}
-
-	watchSessions := make([]*fswatch.Session, len(watchOpts))
-	activeWatchOpts = make([]*watchOpt, len(watchOpts))
-	for i, watchOpt := range watchOpts {
-		session, err := createSession(&watchOpt, workerOpts)
-		if err != nil {
-			logger.Error("unable to start watcher", zap.Strings("dirs", watchOpt.dirs))
-			return err
-		}
-		watchSessions[i] = session
-		activeWatchOpts[i] = &watchOpt
-		go session.Start()
+	activeWatcher = &watcher{workerOpts: workerOpts}
+	err := activeWatcher.startWatching(watchOpts)
+	if err != nil {
+		return err
 	}
-
 	reloadWaitGroup = sync.WaitGroup{}
 	blockReloading.Store(false)
+
 	return nil
 }
 
-func createSession(watchOpt *watchOpt, workerOpts []workerOpt) (*fswatch.Session, error) {
+func drainWatcher() {
+	if(activeWatcher == nil) {
+        return
+    }
+	logger.Info("stopping watcher...")
+	blockReloading.Store(true)
+	activeWatcher.stopWatching()
+	reloadWaitGroup.Wait()
+	activeWatcher = nil
+}
+
+func (w *watcher) startWatching(watchOpts []watchOpt) error {
+	w.sessions = make([]*fswatch.Session, len(watchOpts))
+	w.watchOpts = make([]*watchOpt, len(watchOpts))
+	for i, watchOpt := range watchOpts {
+        session, err := createSession(&watchOpt)
+        if err != nil {
+            logger.Error("unable to watch dirs", zap.Strings("dirs", watchOpt.dirs))
+            return err
+        }
+		w.watchOpts[i] = &watchOpt
+        w.sessions[i] = session
+        go session.Start()
+    }
+	return nil
+}
+
+func (w *watcher) stopWatching() {
+	for i, session := range w.sessions {
+        w.watchOpts[i].isActive = false
+        if err := session.Stop(); err != nil {
+            logger.Error("failed to stop watcher", zap.Error(err))
+        }
+        if err := session.Destroy(); err != nil {
+            logger.Error("failed to destroy watcher", zap.Error(err))
+        }
+    }
+}
+
+func createSession(watchOpt *watchOpt) (*fswatch.Session, error) {
 	opts := []fswatch.Option{
 		fswatch.WithRecursive(watchOpt.isRecursive),
 		fswatch.WithFollowSymlinks(watchOpt.followSymlinks),
@@ -50,56 +85,34 @@ func createSession(watchOpt *watchOpt, workerOpts []workerOpt) (*fswatch.Session
 		fswatch.WithMonitorType((fswatch.MonitorType)(watchOpt.monitorType)),
 		fswatch.WithFilters(watchOpt.filters),
 	}
-	handleFileEvent := registerFileEvent(watchOpt, workerOpts)
+	handleFileEvent := registerFileEvent(watchOpt)
 	logger.Debug("starting watcher session", zap.Strings("dirs", watchOpt.dirs))
 	return fswatch.NewSession(watchOpt.dirs, handleFileEvent, opts...)
 }
 
-func drainWatcher() {
-	stopWatcher()
-	reloadWaitGroup.Wait()
-}
-
-func stopWatcher() {
-	logger.Info("stopping watcher...")
-	blockReloading.Store(true)
-	for _, session := range watchSessions {
-		if err := session.Stop(); err != nil {
-			logger.Error("failed to stop watcher", zap.Error(err))
-		}
-		if err := session.Destroy(); err != nil {
-			logger.Error("failed to destroy watcher", zap.Error(err))
-		}
-	}
-	// we also need to deactivate the watchOpts to avoid a race condition in tests
-	for _, watchOpt := range activeWatchOpts {
-		watchOpt.isActive = false
-	}
-}
-
-func registerFileEvent(watchOpt *watchOpt, workerOpts []workerOpt) func([]fswatch.Event) {
+func registerFileEvent(watchOpt *watchOpt) func([]fswatch.Event) {
 	return func(events []fswatch.Event) {
 		for _, event := range events {
-			if handleFileEvent(event, watchOpt, workerOpts) {
+			if handleFileEvent(event, watchOpt) {
 				break
 			}
 		}
 	}
 }
 
-func handleFileEvent(event fswatch.Event, watchOpt *watchOpt, workerOpts []workerOpt) bool {
+func handleFileEvent(event fswatch.Event, watchOpt *watchOpt) bool {
 	if !watchOpt.allowReload(event.Path) || !blockReloading.CompareAndSwap(false, true) {
 		return false
 	}
 	logger.Info("filesystem change detected, restarting workers...", zap.String("path", event.Path))
-	go triggerWorkerReload(workerOpts)
+	go triggerWorkerReload()
 
 	return true
 }
 
-func triggerWorkerReload(workerOpts []workerOpt) {
+func triggerWorkerReload() {
 	reloadWaitGroup.Add(1)
-	restartWorkers(workerOpts)
+	restartWorkers(activeWatcher.workerOpts)
 	reloadWaitGroup.Done()
 	blockReloading.Store(false)
 }
