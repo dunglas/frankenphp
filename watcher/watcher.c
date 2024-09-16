fe978c0c88e8e70983e7243483cdff08f337c1a4
a.stecher <a.stecher@sportradar.com>
2024-09-16 17:18:03 +0200

    Fixes linting.

--- watcher/watcher.c
+++ watcher/watcher.c
@@ -2,8 +2,8 @@
 #include "watcher-c.h"
 
 void process_event(struct wtr_watcher_event event, void *data) {
-  go_handle_file_watcher_event((char *)event.path_name, event.effect_type, event.path_type,
-                  (uintptr_t)data);
+  go_handle_file_watcher_event((char *)event.path_name, event.effect_type,
+                               event.path_type, (uintptr_t)data);
 }
 
 void *start_new_watcher(char const *const path, uintptr_t data) {
