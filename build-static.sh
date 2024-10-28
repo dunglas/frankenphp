c0080b4177acd148b4ad5d2145e27cb8a46fa7e9
Kévin Dunglas <kevin@dunglas.fr>
2024-10-28 21:34:44 +0100

    wip

--- build-static.sh
+++ build-static.sh
@@ -122,13 +122,13 @@ fi
 
 # Compile e-dant/watcher as a static library
 mkdir watcher
-RUN curl -s https://api.github.com/repos/e-dant/watcher/releases/latest | \
-		grep tarball_url | \
-		awk '{ print $2 }' | \
-		sed 's/,$//' | \
-		sed 's/"//g' | \
-		xargs curl -L | \
-    tar xz --strip-components 1
+curl -s https://api.github.com/repos/e-dant/watcher/releases/latest | \
+	grep tarball_url | \
+	awk '{ print $2 }' | \
+	sed 's/,$//' | \
+	sed 's/"//g' | \
+	xargs curl -L | \
+	tar xz --strip-components 1
 cd watcher/watcher-c
 cc -c -o libwatcher-c.o ./src/watcher-c.cpp -I ./include -I ../include -std=c++17 -Wall -Wextra -fPIC
 ar rcs libwatcher-c.a libwatcher-c.o
