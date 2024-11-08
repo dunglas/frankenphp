31d678c25e25d5fa12fc9dd310a404d0ad00deac
Alliballibaba <alliballibaba@gmail.com>
2024-11-08 17:12:29 +0100

    Changes level to info.

--- internal/watcher/watcher.go
+++ internal/watcher/watcher.go
@@ -84,7 +84,7 @@ func retryWatching(watchPattern *watchPattern) {
 	if watchPattern.failureCount >= maxFailureCount {
 		return
 	}
-	logger.Debug("watcher was closed prematurely, retrying...", zap.String("dir", watchPattern.dir))
+	logger.Info("watcher was closed prematurely, retrying...", zap.String("dir", watchPattern.dir))
 
 	watchPattern.failureCount++
 	session, err := startSession(watchPattern)
