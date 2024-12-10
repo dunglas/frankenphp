38f87b7b7b1239b0b55f8111e249ee84661ea474
Alliballibaba <alliballibaba@gmail.com>
2024-12-10 23:15:48 +0100

    Adds comment.

--- testdata/transition-worker-2.php
+++ testdata/transition-worker-2.php
@@ -2,6 +2,7 @@
 
 while (frankenphp_handle_request(function () {
     echo "Hello from worker 2";
+    // Simulate work to force potential race conditions (phpmainthread_test.go)
     usleep(1000);
 })) {
 
