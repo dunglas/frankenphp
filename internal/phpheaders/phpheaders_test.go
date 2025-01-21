84d4a92b551bbf9d89d4f0bf015a0651721370e0
Alliballibaba <alliballibaba@gmail.com>
2025-01-21 10:51:35 +0100

    ?Fixes assertion.

--- internal/phpheaders/phpheaders_test.go
+++ internal/phpheaders/phpheaders_test.go
@@ -10,6 +10,6 @@ func TestAllHeadersAreCorrect(t *testing.T) {
 	for header, phpHeader := range commonRequestHeaders {
 		expectedPHPHeader := GetUnCommonHeader(header)
 		hardCodedHeader := phpHeader + "\x00"
-		assert.Equal(t, hardCodedHeader, expectedPHPHeader, "header %s is not well formed", header)
+		assert.Equal(t, hardCodedHeader, expectedPHPHeader, "header is not well formed: "+phpHeader)
 	}
 }
