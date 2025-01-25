bc6587435594cd5dc74893b0ffadc496531f6ea7
Alliballibaba <alliballibaba@gmail.com>
2025-01-25 15:29:38 +0100

    clang-format

--- frankenphp.c
+++ frankenphp.c
@@ -442,18 +442,21 @@ PHP_FUNCTION(frankenphp_handle_request) {
   ctx->has_active_request = false;
   go_frankenphp_finish_worker_request(thread_index);
 
-  /* free any php_stream resources that are not php source files and are unreferenced
-   PHP resources are stored in the thread-local HashTable EG(regular_list) */
+  /* free any php_stream resources that are not php source files and are
+   unreferenced PHP resources are stored in the thread-local HashTable
+   EG(regular_list) */
   zend_resource *val;
   ZEND_HASH_FOREACH_PTR(&EG(regular_list), val) {
     /* verify the resource is a stream */
     if (val->type == php_file_le_stream()) {
       php_stream *stream = (php_stream *)val->ptr;
-      if (stream != NULL && stream->ops != &php_stream_stdio_ops && GC_REFCOUNT(val) == 1) {
+      if (stream != NULL && stream->ops != &php_stream_stdio_ops &&
+          GC_REFCOUNT(val) == 1) {
         zend_list_delete(val);
       }
     }
-  } ZEND_HASH_FOREACH_END();
+  }
+  ZEND_HASH_FOREACH_END();
 
   RETURN_TRUE;
 }
