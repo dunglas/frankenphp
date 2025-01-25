cb5dc930dd4abc923bab2171f45c6e0573a423ea
Alliballibaba <alliballibaba@gmail.com>
2025-01-25 15:29:07 +0100

    Test

--- frankenphp.c
+++ frankenphp.c
@@ -442,8 +442,18 @@ PHP_FUNCTION(frankenphp_handle_request) {
   ctx->has_active_request = false;
   go_frankenphp_finish_worker_request(thread_index);
 
-  zend_hash_graceful_reverse_destroy(&EG(regular_list));
-  zend_init_rsrc_list();
+  /* free any php_stream resources that are not php source files and are unreferenced
+   PHP resources are stored in the thread-local HashTable EG(regular_list) */
+  zend_resource *val;
+  ZEND_HASH_FOREACH_PTR(&EG(regular_list), val) {
+    /* verify the resource is a stream */
+    if (val->type == php_file_le_stream()) {
+      php_stream *stream = (php_stream *)val->ptr;
+      if (stream != NULL && stream->ops != &php_stream_stdio_ops && GC_REFCOUNT(val) == 1) {
+        zend_list_delete(val);
+      }
+    }
+  } ZEND_HASH_FOREACH_END();
 
   RETURN_TRUE;
 }
@@ -749,7 +759,7 @@ void frankenphp_register_variables_from_request_info(
     zend_string *path_translated, zend_string *query_string,
     zend_string *auth_user, zend_string *request_method) {
   frankenphp_register_variable_from_request_info(
-      content_type, (char *)SG(request_info).content_type, false,
+      content_type, (char *)SG(request_info).content_type, true,
       track_vars_array);
   frankenphp_register_variable_from_request_info(
       path_translated, (char *)SG(request_info).path_translated, false,
