9c3554a24b8c9b8e37deebc8c55fd821f67f185f
DubbleClick <m@pyc.ac>
2025-04-16 10:56:08 +0700

    dnf over yum, I think the yum alias was removed in RH 9.5

--- package/after_install.sh
+++ package/after_install.sh
@@ -6,7 +6,7 @@ else
 	if [ -f /etc/debian_version ]; then
 		echo "Warning: setcap not found. Install it with: sudo apt install libcap2-bin"
 	elif [ -f /etc/redhat-release ]; then
-		echo "Warning: setcap not found. Install it with: sudo yum install libcap"
+		echo "Warning: setcap not found. Install it with: sudo dnf install libcap"
 	else
 		echo "Warning: setcap not found. Install the appropriate libcap package for your system."
 	fi
