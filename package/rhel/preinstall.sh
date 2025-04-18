9ac49d42ca3616512b7232ce715eef9a7df01fb5
DubbleClick <m@pyc.ac>
2025-04-18 21:12:31 +0700

    linter again

--- package/rhel/preinstall.sh
+++ package/rhel/preinstall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-getent group frankenphp &>/dev/null || \
+getent group frankenphp &>/dev/null ||
 	groupadd -r frankenphp &>/dev/null
-getent passwd frankenphp &>/dev/null || \
+getent passwd frankenphp &>/dev/null ||
 	useradd -r -g frankenphp -d /var/lib/frankenphp -s /sbin/nologin -c 'FrankenPHP web server' frankenphp &>/dev/null
 exit 0
