ea0b7c4721e2bfd1186df8ce3a130434e1084b95
DubbleClick <m@pyc.ac>
2025-04-18 21:08:16 +0700

    linter

--- package/rhel/postinstall.sh
+++ package/rhel/postinstall.sh
@@ -11,16 +11,16 @@ if [ -x /usr/sbin/getsebool ]; then
 fi
 if [ -x /usr/sbin/semanage ] && [ -x /usr/sbin/restorecon ]; then
 	# file contexts
-	semanage fcontext --add --type httpd_exec_t        '/usr/bin/frankenphp'		     2> /dev/null || :
-	semanage fcontext --add --type httpd_sys_content_t '/usr/share/frankenphp(/.*)?' 2> /dev/null || :
-	semanage fcontext --add --type httpd_config_t	     '/etc/frankenphp(/.*)?'	     2> /dev/null || :
-	semanage fcontext --add --type httpd_var_lib_t	   '/var/lib/frankenphp(/.*)?'   2> /dev/null || :
+	semanage fcontext --add --type httpd_exec_t '/usr/bin/frankenphp' 2>/dev/null || :
+	semanage fcontext --add --type httpd_sys_content_t '/usr/share/frankenphp(/.*)?' 2>/dev/null || :
+	semanage fcontext --add --type httpd_config_t '/etc/frankenphp(/.*)?' 2>/dev/null || :
+	semanage fcontext --add --type httpd_var_lib_t '/var/lib/frankenphp(/.*)?' 2>/dev/null || :
 	restorecon -r /usr/bin/frankenphp /usr/share/frankenphp /etc/frankenphp /var/lib/frankenphp || :
 fi
 if [ -x /usr/sbin/semanage ]; then
 	# QUIC
-	semanage port --add --type http_port_t --proto udp 80   2> /dev/null || :
-	semanage port --add --type http_port_t --proto udp 443  2> /dev/null || :
+	semanage port --add --type http_port_t --proto udp 80 2>/dev/null || :
+	semanage port --add --type http_port_t --proto udp 443 2>/dev/null || :
 	# admin endpoint
-	semanage port --add --type http_port_t --proto tcp 2019 2> /dev/null || :
+	semanage port --add --type http_port_t --proto tcp 2019 2>/dev/null || :
 fi
