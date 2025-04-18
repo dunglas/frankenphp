2127d031f2ee1f631ca0d470b4c4cc61bf04deae
DubbleClick <m@pyc.ac>
2025-04-18 20:59:41 +0700

    tabs

--- package/debian/prerm.sh
+++ package/debian/prerm.sh
@@ -2,6 +2,6 @@
 set -e
 
 if [ -d /run/systemd/system ] && [ "$1" = remove ]; then
-        deb-systemd-invoke stop frankenphp.service >/dev/null || true
-        deb-systemd-invoke stop frankenphp-api.service >/dev/null || true
+	deb-systemd-invoke stop frankenphp.service >/dev/null || true
+	deb-systemd-invoke stop frankenphp-api.service >/dev/null || true
 fi
