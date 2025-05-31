66cdb8d14556e4d1da4bf12596e2db329ecbd972
Robert Landers <landers.robert@gmail.com>
2025-05-31 11:34:41 +0200

    fix test

--- testdata/only-headers.php
+++ testdata/only-headers.php
@@ -1,6 +1,11 @@
 <?php
-header('Content-Type: application/json');
-header('HTTP/1.1 204 No Content', true, 204);
 
-echo '{"status": "test"}';
-flush();
+require_once __DIR__.'/_executor.php';
+
+return function () {
+    header('Content-Type: application/json');
+    header('HTTP/1.1 204 No Content', true, 204);
+
+    echo '{"status": "test"}';
+    flush();
+};
