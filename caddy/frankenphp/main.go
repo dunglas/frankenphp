ec8bf65cb40aaafd4372c3768eefcfc125ecddd8
Robert Landers <landers.robert@gmail.com>
2025-06-07 09:59:56 +0200

    allow installing an embedded php-cli

--- caddy/frankenphp/main.go
+++ caddy/frankenphp/main.go
@@ -2,9 +2,6 @@ package main
 
 import (
 	caddycmd "github.com/caddyserver/caddy/v2/cmd"
-	"os"
-	"path/filepath"
-
 	// plug in Caddy modules here.
 	_ "github.com/caddyserver/caddy/v2/modules/standard"
 	_ "github.com/dunglas/caddy-cbrotli"
@@ -14,10 +11,5 @@ import (
 )
 
 func main() {
-	// if the name of the current program is `php`, then call into the php-cli currently embedded
-	programName := filepath.Base(os.Args[0])
-	if programName == "php" {
-		runPhpCli() // never returns for static builds with INCLUDE_CLI set
-	}
 	caddycmd.Main()
 }
--- caddy/frankenphp/php-cli-stub.go
+++
@@ -1,6 +0,0 @@
-//go:build !include_php_cli
-
-package main
-
-func runPhpCli() {
-}
