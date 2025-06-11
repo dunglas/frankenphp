d7e76a14d9dde1c45f2b7941acdb687d16f1f427
Kévin Dunglas <kevin@dunglas.fr>
2025-06-11 16:20:32 +0200

    feat: add helpers to create PHP extensions (#1644)
    * feat: add helpers to create PHP extensions
    
    * cs
    
    * feat: GoString
    
    * test
    
    * add test for RegisterExtension
    
    * cs
    
    * optimize includes
    
    * fix

--- frankenphp.h
+++ frankenphp.h
@@ -2,6 +2,7 @@
 #define _FRANKENPPHP_H
 
 #include <Zend/zend_types.h>
+#include <Zend/zend_modules.h>
 #include <stdbool.h>
 #include <stdint.h>
 
@@ -91,5 +92,7 @@ void frankenphp_register_bulk(
     ht_key_value_pair server_software, ht_key_value_pair http_host,
     ht_key_value_pair auth_type, ht_key_value_pair remote_ident,
     ht_key_value_pair request_uri);
+
+void register_extensions(zend_module_entry *m, int len);
 
 #endif
