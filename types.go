54468fa6f13c8dd958c0b84bd9ee76956d21aae7
Alexandre Daubois <alex.daubois@gmail.com>
2025-06-12 17:50:05 +0200

    revert types files

---
+++ types.go
@@ -0,0 +1,45 @@
+package frankenphp
+
+//#include <zend.h>
+import "C"
+import "unsafe"
+
+// EXPERIMENTAL: GoString converts a zend_string to a Go string without copy.
+func GoString(s unsafe.Pointer) string {
+	if s == nil {
+		return ""
+	}
+
+	zendStr := (*C.zend_string)(s)
+
+	return C.GoStringN((*C.char)(unsafe.Pointer(&zendStr.val)), C.int(zendStr.len))
+}
+
+// EXPERIMENTAL: GoStringCopy converts a zend_string to a Go string with copy.
+func GoStringCopy(s unsafe.Pointer) string {
+	if s == nil {
+		return ""
+	}
+
+	zendStr := (*C.zend_string)(s)
+	bytes := C.GoBytes(unsafe.Pointer(&zendStr.val), C.int(zendStr.len))
+
+	return string(bytes)
+}
+
+// EXPERIMENTAL: PHPString converts a Go string to a zend_string with copy. The string can be
+// non-persistent (automatically freed after the request by the ZMM) or persistent. If you choose
+// the second mode, it is your repsonsability to free the allocated memory.
+func PHPString(s string, persistent bool) unsafe.Pointer {
+	if s == "" {
+		return nil
+	}
+
+	zendStr := C.zend_string_init(
+		(*C.char)(unsafe.Pointer(unsafe.StringData(s))),
+		C.size_t(len(s)),
+		C._Bool(persistent),
+	)
+
+	return unsafe.Pointer(zendStr)
+}
