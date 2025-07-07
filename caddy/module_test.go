0751ccfa0f2d572bba8e2990bd9a62652835f43d
Alliballibaba <alliballibaba@gmail.com>
2025-07-07 21:22:40 +0200

    Adds more test cases.

--- caddy/module_test.go
+++ caddy/module_test.go
@@ -14,27 +14,40 @@ func TestRootBehavesTheSameOutsideAndInsidePhpServer(t *testing.T) {
 	testPortNum, _ := strconv.Atoi(testPort)
 	testPortTwo := strconv.Itoa(testPortNum + 1)
 	expectedFileResponse, _ := os.ReadFile("../testdata/files/static.txt")
+	hostWithRootOutside := "http://localhost:" + testPort
+	hostWithRootInside := "http://localhost:" + testPortTwo
 	tester.InitServer(`
 		{
 			skip_install_trust
 			admin localhost:2999
+			debug
 		}
 
-		http://localhost:`+testPort+` {
-			root ../testdata/files
+		`+hostWithRootOutside+` {
+			root ../testdata
 			php_server
 		}
 
-		http://localhost:`+testPortTwo+` {
+		`+hostWithRootInside+` {
 			php_server {
-				root ../testdata/files
+				root ../testdata
 			}
 		}
 		`, "caddyfile")
 
-	// serve the file with root outside of php_server
-	tester.AssertGetResponse("http://localhost:"+testPort+"/static.txt", http.StatusOK, string(expectedFileResponse))
+	// serve a static file
+	tester.AssertGetResponse(hostWithRootOutside+"/files/static.txt", http.StatusOK, string(expectedFileResponse))
+	tester.AssertGetResponse(hostWithRootInside+"/files/static.txt", http.StatusOK, string(expectedFileResponse))
 
-	// serve the file with root within php_server
-	tester.AssertGetResponse("http://localhost:"+testPortTwo+"/static.txt", http.StatusOK, string(expectedFileResponse))
+	// serve a php file
+	tester.AssertGetResponse(hostWithRootOutside+"/hello.php", http.StatusOK, "Hello from PHP")
+	tester.AssertGetResponse(hostWithRootInside+"/hello.php", http.StatusOK, "Hello from PHP")
+
+	// fallback to index.php
+	tester.AssertGetResponse(hostWithRootOutside+"/some-path", http.StatusOK, "I am by birth a Genevese (i not set)")
+	tester.AssertGetResponse(hostWithRootInside+"/some-path", http.StatusOK, "I am by birth a Genevese (i not set)")
+
+	// fallback to directory index ('dirIndex' in module.go)
+	tester.AssertGetResponse(hostWithRootOutside+"/files/", http.StatusOK, "Hello from directory index.php")
+	tester.AssertGetResponse(hostWithRootInside+"/files/", http.StatusOK, "Hello from directory index.php")
 }
---
+++ testdata/files/index.php
@@ -0,0 +1,3 @@
+<?php
+
+echo "Hello from directory index.php";
