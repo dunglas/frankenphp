647d68912a294367b0fe96d93a33f308b5951469
Alexandre Daubois <alex.daubois@gmail.com>
2025-08-01 09:01:24 +0200

    fix(types): better zval handling to avoid leaks with arrays

--- types.h
+++ types.h
@@ -14,4 +14,12 @@ void *__emalloc__(size_t size);
 void __zend_hash_init__(HashTable *ht, uint32_t nSize, dtor_func_t pDestructor,
                         bool persistent);
 
+void __zval_null__(zval *zv);
+void __zval_bool__(zval *zv, bool val);
+void __zval_long__(zval *zv, zend_long val);
+void __zval_double__(zval *zv, double val);
+void __zval_string__(zval *zv, zend_string *str);
+void __zval_arr__(zval *zv, zend_array *arr);
+zend_array *__zend_new_array__(uint32_t size);
+
 #endif
