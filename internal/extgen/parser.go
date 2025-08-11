17eba05bcd2f2d84cb3ed70aebb086ac2470a48f
Robert Landers <landers.robert@gmail.com>
2025-08-11 19:33:34 +0200

    remove EXPERIMENTAL
    Signed-off-by: Robert Landers <landers.robert@gmail.com>

--- internal/extgen/parser.go
+++ internal/extgen/parser.go
@@ -2,31 +2,26 @@ package extgen
 
 type SourceParser struct{}
 
-// EXPERIMENTAL
 func (p *SourceParser) ParseFunctions(filename string) ([]phpFunction, error) {
 	functionParser := &FuncParser{}
 	return functionParser.parse(filename)
 }
 
-// EXPERIMENTAL
 func (p *SourceParser) ParseClasses(filename string) ([]phpClass, error) {
 	classParser := classParser{}
 	return classParser.parse(filename)
 }
 
-// EXPERIMENTAL
 func (p *SourceParser) ParseConstants(filename string) ([]phpConstant, error) {
 	constantParser := &ConstantParser{}
 	return constantParser.parse(filename)
 }
 
-// EXPERIMENTAL
 func (p *SourceParser) ParseNamespace(filename string) (string, error) {
 	namespaceParser := NamespaceParser{}
 	return namespaceParser.parse(filename)
 }
 
-// EXPERIMENTAL
 func (p *SourceParser) ParseModule(filename string) (*phpModule, error) {
 	moduleParser := &ModuleParser{}
 	return moduleParser.parse(filename)
